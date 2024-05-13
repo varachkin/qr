f5167ac96bf3cf8e589884f236814b40e1ffdf3d
Aliaksandr Varachkin <a.varachkin@vendorobotics.com>
2024-05-13 16:00:49 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import { useState } from 'react';
 function App() {
   const [active, setActive] = useState(false);
   const [text, setText] = useState('CONFIRM')
+  const [title, setTitle] = useState('Confirm payment')
 
-  const handleConfirm = () => {
+  function getQueryParams(url) {
+    const queryParams = {};
+    const urlSearchParams = new URLSearchParams(url.split('?')[1]);
+    for (const [key, value] of urlSearchParams.entries()) {
+      queryParams[key] = value;
+    }
+    return queryParams;
+  }
+
+  const handleConfirm = (event) => {
+    event.preventDefault();
+    const queryParams = getQueryParams(event.target.href);
+    setTitle(JSON.parse(queryParams))
     setActive(true)
     setText('CONFIRMED')
     setTimeout(()=> {
@@ -20,7 +33,7 @@ function App() {
       <header></header>
       <main>
         <div className='logo-main'></div>
-        <p className='title'>Confirm payment</p>
+        <p className='title'>{title}</p>
         <p className={`${active ? 'active' : ''}`}>
           <button
             className={`waves`}
